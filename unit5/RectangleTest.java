ea3bcb6ede384eb4d782c11bca2f2b822f9e2dd2
elissaliufah <elissa.liu28@gmail.com>
2025-12-01 14:42:53 -0800

    k

---
+++ unit5/RectangleTest.java
@@ -0,0 +1,12 @@
+public class RectangleTest{
+	public static void main(String[] args){
+		Rectangle rect = new Rectangle(3,4);
+		
+		System.out.println(rect.getBase());
+		System.out.println(rect.getHeight());
+		System.out.println(rect.area());
+		System.out.println(rect.perimeter());
+		System.out.println(rect.diagonal());
+	}
+
+}
